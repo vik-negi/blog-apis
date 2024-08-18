38162488cbd249925bd56b9ae6c94d0b9d4c1b71
vik-negi <vikramnegi175@gmail.com>
2024-08-18 20:51:17 +0530

    docker-composed change

--- src/main/java/com/example/blog_app/BlogAppApplication.java
+++ src/main/java/com/example/blog_app/BlogAppApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogAppApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		String secretKey = dotenv.get("SECRET_KEY");
 
 		SpringApplication.run(BlogAppApplication.class, args);
 	}
